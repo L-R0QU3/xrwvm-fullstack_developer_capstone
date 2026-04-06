248e1f5a64bbb0b6aa24fbbed9354d09bae4053e
L-R0QU3 <luis.roquetic@gmail.com>
2026-04-05 22:26:45 -0400

    Fix linting errors

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,13 +1,24 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import Dealers from './components/Dealers/Dealers';
+import Dealer from './components/Dealers/Dealer';
+import PostReview from './components/Dealers/PostReview';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dealers" element={<Dealers />} />
+        <Route path="/dealer/:id" element={<Dealer />} />
+        <Route path="/postreview/:id" element={<PostReview />} />
+        <Route path="/" element={<div>Home (temp)</div>} />
+      </Routes>
+    </Router>
   );
 }
+
 export default App;
